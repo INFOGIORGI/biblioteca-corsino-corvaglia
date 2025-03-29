5ce3fcf53178e45bd549a1bf1baa323c3b3b0c39
Alberto Corvaglia <albertocorvaglia8@gmail.com>
2025-03-29 23:21:27 +0100

    cose toste

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ Session(app)
 
 @app.route("/")
 def home():
+    if session.get("username"):
+        session["isFirstLogin"] = getCredenziali(mysql, session["username"])[-1]
     return render_template("home.html", titolo ="Home")
 
 @app.route("/login/", methods = ["GET", "POST"])
@@ -51,7 +53,8 @@ def login():
                 return redirect(url_for("login"))
         session["username"] = user
         session["userType"] = account[6]
-        print(session)
+        session["isFirstLogin"] = account[-1]
+        
         return redirect(url_for("home"))
     
 
@@ -65,7 +68,6 @@ def register():
     else:
         nome = request.form.get("nome")
         cognome = request.form.get("cognome")
-        user = request.form.get("username")
         data = request.form.get("data")
         email = request.form.get("email")
         if session["userType"] == "admin":  
@@ -74,30 +76,64 @@ def register():
             tipo = "utente"
        
 
-        if(user == "" or nome =="" or cognome == "" or data == "" or email ==""):
+        if nome == "" or cognome == "" or data == "" or email =="":
             flash("Tutti i campi devono essere completi")
             return redirect(url_for("register"))
         
-        check = getCredenziali(mysql, user)
-        if check != None:
-            flash("L'username è già utilizzato")
-            return redirect(url_for("register"))
-        str = nome + data + cognome #formata da nome, data di nascita, cognome (es. Alberto2006-08-20Corvaglia)
-        pw = generate_password_hash(str)
+        genPwd = nome.lower() + cognome.lower() + data.lower() #formata da nome, data di nascita, cognome (es. AlbertoCorvaglia2006-08-20)
+        pw = generate_password_hash(genPwd)
         
-        doRegister(mysql, nome, cognome, user,pw, data, email, tipo)
+        user = nome+cognome
+        
+        sameNameUsers = getSameNameUsers(mysql, nome,cognome)[0]
+        if sameNameUsers>0:
+            user+=str(sameNameUsers)
+        
+        print(user)
+        doRegister(mysql, user, nome, cognome, pw, data, email, tipo)
 
-        return redirect(url_for("home"))
+        return redirect(url_for(session["userType"]))
    
+@app.route("/password-update/", methods=["POST", "GET"])
+def passwordUpdate():
+    
+    if not session["isFirstLogin"]:
+        flash("Non è il primo login")
+        return redirect(url_for("home"))
+    password = request.form.get("password","")
+    password_confirm = request.form.get("confirm","")
+    print(password, password_confirm)
+    if password == "" or password_confirm == "":
+            flash("Tutti i campi devono essere completi")
+            return redirect(url_for("passwordUpdate"))
+    if password_confirm != password:
+        flash("Le password non corrispondono")
+        return redirect(url_for("passwordUpdate"))
+
+    username = session["username"]
+    password = generate_password_hash(password)
+    updatePassword(mysql, username, password)
+
+    
+    return redirect(url_for("home"))
 
 @app.route("/admin/")
 def admin():
-    return render_template("admin.html")      
-
-@app.route("/catalogo/")
-def catalogo():
-    return render_template("catalogo.html")     
-
+    
+    if session.get("userType")=="admin":
+        users = getUsers(mysql)
+        return render_template("admin.html",users=users) 
+    else:
+        return abort(403)    
+   
+@app.route("/bibliotecario/")
+def bibliotecario():
+    if session.get("userType")=="bibliotecario":
+        users = getUsers(mysql)
+        return render_template("bibliotecario.html",users=users) 
+    else:
+        return abort(403)    
+   
 @app.route("/logout/")
 def logout():
     session.clear()
--- db.py
+++ db.py
@@ -1,3 +1,19 @@
+def getUsers(mysql):
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM UTENTE"
+    cursor.execute(query)
+    query = cursor.fetchall()
+    cursor.close()
+    return query
+
+def getSameNameUsers(mysql,nome,cognome):
+    cursor = mysql.connection.cursor()
+    query = "SELECT COUNT(*) FROM UTENTE WHERE Nome = %s and Cognome = %s"
+    cursor.execute(query,(nome,cognome))
+    query = cursor.fetchone()
+    cursor.close()
+    return query
+
 def getCredenziali(mysql, username):
         cursor = mysql.connection.cursor()
         query = "SELECT * FROM UTENTE WHERE username = %s"
@@ -6,8 +22,14 @@ def getCredenziali(mysql, username):
         cursor.close()
         return account
 
-def doRegister(mysql, nome, cognome, user, pw, data, email, tipo):
+def doRegister(mysql, user, nome, cognome, pw, data, email, tipo):
     cursor = mysql.connection.cursor()
     query = "INSERT INTO UTENTE VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
-    cursor.execute(query,(nome,cognome,user,pw,data,email,tipo,1))
+    cursor.execute(query,(user,nome,cognome,pw,data,email,tipo,1))
+    mysql.connection.commit()
+    
+def updatePassword(mysql, username, password):
+    cursor = mysql.connection.cursor()
+    query = "UPDATE UTENTE SET PASSWORD = %s, firstLogin = false WHERE USERNAME = %s"
+    cursor.execute(query,(password, username))
     mysql.connection.commit()
